8cf40cae0212b54bf3b9d91282da899aca349b6d
rashtecq <jhalli4me@gmail.com>
2022-11-22 14:19:16 +0000

    Adding kubesect step in jenkins and kubesec-scan.sh file

---
+++ kubesec-scan.sh
@@ -0,0 +1,22 @@
+#Using kubesec v2  API for scanning
+scan_result=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
+scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan| jq .[0].message -r)
+scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan| jq .[0].score)
+
+
+#Using kubesec docker image for scanning
+#scan_result=$(docker run -i kubesec/kubesec:512c5e0 scan /dev/stdin < k8s_deployment_service.yaml)
+#scan_message=$(docker run -i kubesec/kubesec:512c5e0 scan /dev/stdin < k8s_deployment_service.yaml| jq .[0].message -r)
+#scan_score=$(docker run -i kubesec/kubesec:512c5e0 scan /dev/stdin < k8s_deployment_service.yaml| jq .[0].score)
+
+#kubesec scan result processing 
+# echo "scan_score : $scan_score"
+if [["${scan_score}" -ge 5 ]]; then 
+    echo "Score is $scan_score"
+    echo "kubesec Scan $scan_message"
+else
+    echo "Score is $scan_score, which is less then or equal to 5"
+    echo "Scaning kubernetes Resource has failed"
+    exit 1;
+fi;
+    
