db1469f66dd474b789b4dee3564ff6d98491d05f
rashtecq <jhalli4me@gmail.com>
2022-11-23 17:50:24 +0000

    Fixing zap.sh

--- zap.sh
+++ zap.sh
@@ -13,7 +13,7 @@ sudo mv zap_report.html owasp-zap-report
 
 
 echo "Exit Code : $exit_code"
-if [[ ${exit_code -ne 0 }]]; then
+if [[ "${exit_code}" -ne 0 ]]; then
     echo "OWASP report has either Low/Medium/High Risk. Please check the HTML report"
     exit 1;
     else
