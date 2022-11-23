645b47676735aaad7e73f6f48102890bb799d537
rashtecq <jhalli4me@gmail.com>
2022-11-23 17:57:36 +0000

    Corrextion in zap.sh

--- zap.sh
+++ zap.sh
@@ -3,7 +3,7 @@ PORT=$(kubectl -n default get svc ${service_name} -o json | jq .spec.ports[].nod
 
 chmod 777 $(pwd)
 echo $(id -u):$(id -g)
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openssl -r zap_report.html
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
 
 exit_code=$?
 
